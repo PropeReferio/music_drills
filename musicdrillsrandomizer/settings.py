aa55daa1e0c1a0cd4984cbad305f7052c848f2b1
PropeReferio <lemuel.b.stevens@gmail.com>
2024-04-28 22:01:11 -0500

    Prepping for Railway

--- musicdrillsrandomizer/settings.py
+++ musicdrillsrandomizer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-m74+1p_)-6rzk6r+()9aeqikc5=yh#)ekn@4)s9zgq_bwuoe@7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
